602b040f17b1c2203f3eab44357fd54e53230fb8
FALLENau <reece.c.jones@gmail.com>
2017-06-27 17:22:34 +0100

    camera can take photo testpass

--- homework/Camera.java
+++ homework/Camera.java
@@ -5,11 +5,14 @@ public class Camera{
   private String name;
   private String noise;
   private String resolution;
+  private ArrayList<Storage> rell;
+
 
   public Camera(String name, String noise, String resolution){
     this.name = name;
     this.noise = noise;
     this.resolution = resolution;
+    this.rell = new ArrayList<Storage>();
 
   }
 
@@ -24,6 +27,14 @@ public class Camera{
   public String getResolution(){
     return this.resolution;
   }
+
+  public int photoCount(){
+    return this.rell.size();
+  }
+
+  public void take(Storage photo){
+   this.rell.add(photo);
+ }
 
 
 }
--- homework/CameraTest.java
+++ homework/CameraTest.java
@@ -3,11 +3,13 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.*;
 
 public class CameraTest{
+  Photo photo;
   Camera camera1;
   Camera camera2;
 
   @Before
   public void before() {
+    photo = new Photo();
     camera1 = new Camera("E.G 1. Nikon 34T", "Noise reduction filter", "Hi-resolution");
     camera2 = new Camera("Sony ILCE5100L", "No noise reduction", "Lo-resolution");
 
@@ -28,4 +30,9 @@ public class CameraTest{
     assertEquals("Hi-resolution", camera1.getResolution());
   }
 
+  @Test //Note:start-build stage one.
+  public void canCamera1TakePhoto(){
+    camera1.take(photo);
+    assertEquals(1, camera1.photoCount());
+  }
 }
